b6eadbf4962d48d5042e3b247f2dbb7da5f9967f
anknits <ankuragarwall04@gmail.com>
2016-10-01 18:39:52 +0000

    timestamp commit

--- server0.js
+++ server0.js
@@ -1,5 +1,4 @@
 var express = require("express")
-var moment = require("moment")
 var app = express();
 var unix = null
 var natural = null
@@ -9,17 +8,18 @@ app.param('para', function(req,res,next,para){
 })
 app.get('/:para', function (req, res) {
   var input = req.para
-  if(moment(input, 'MMMM DD, Y', true).isValid()){
-    var date = moment(input, 'MMMM DD, Y', true)
-    unix = date.unix()
-    natural = input
+  if(input.indexOf(',') != -1){
+    unix = new Date(input).getTime()
+    //natural = input
+    natural = new Date(unix).toDateString()
   }
   else if(!isNaN(input)){
     //unix = new Date(input).getTime()
     unix = Number(input)
-    natural = moment(input*1000).format('MMMM DD, Y')
+    natural = new Date(input*1000).toDateString()
   }
-  res.send(JSON.stringify({"unix": unix, "natural": natural}));
+  
+  res.send(JSON.stringify({"input":input, "unix": unix, "natural": natural}));
 });
 
 app.listen(8080);
